5f4e159a93233f7f228080ad3a070066d88c888e
maribels_are_ringing <maribel.duron86@gmail.com>
2021-04-24 22:55:18 -0700

    added seeds and confirmed db and table creation

--- db/schema.sql
+++ db/schema.sql
@@ -2,22 +2,26 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
 
-CREATE TABLE employees (
+CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(2) NOT NULL,
-    department_id INTEGER
+    department_id INTEGER,
+    CONSTRAINT fk_dept
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
 );
 
-CREATE TABLE departments (
+CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO employees (first_name, last_name)
-VALUES ('Ringo', 'Starr'),
-('John', 'Lennon'),
-('Paul', 'McCartney'),
-('George', 'Harrison');
+INSERT INTO employees (first_name, last_name, manager_id)
+VALUES ('Ringo', 'Starr', 1),
+('John', 'Lennon', 7),
+('Paul', 'McCartney', 9),
+('George', 'Harrison', 5);
 
 INSERT INTO roles (title, salary)
 VALUES ('Drummer', 5,000.00),
@@ -10,8 +10,8 @@ VALUES ('Drummer', 5,000.00),
 ('Bassist', 4,000.00),
 ('Singer', 7,000.00),
 ('Tour Bus Driver', 500.00),
-('Stage Crew', 1,000.00);
-('Manager', 2,000.00)
+('Stage Crew', 1,000.00),
+('Manager', 2,000.00);
 
 INSERT INTO departments (name)
 VALUES ('Band'),
